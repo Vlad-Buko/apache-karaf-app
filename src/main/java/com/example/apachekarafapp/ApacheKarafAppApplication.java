74c913aee5148a13db75e15234396545bf296c39
vladislav_domaniewski <vladislav_buko@mail.ru>
2022-08-21 20:21:01 +0300

    Added start project

---
+++ src/main/java/com/example/apachekarafapp/ApacheKarafAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.apachekarafapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheKarafAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApacheKarafAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apachekarafapp/ApacheKarafAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apachekarafapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheKarafAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
